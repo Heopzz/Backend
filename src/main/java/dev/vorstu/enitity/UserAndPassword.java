5c84b90e88bb1ecc50766a023bc831f46c71fec1
Heopzz <danilf213@gmail.com>
2024-06-20 00:54:20 +0300

    DOMINATOR_VERSION

---
+++ src/main/java/dev/vorstu/enitity/UserAndPassword.java
@@ -0,0 +1,19 @@
+package dev.vorstu.enitity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserAndPassword {
+
+    private User user;
+
+    private String password;
+
+}
